7fcebe9cb4e7e516b10ea3f505cc219e2538b45c
Damian Raczkowski <idontknow5325@gmail.com>
2023-02-26 22:21:54 +0100

    Improvement: Add useOnlineManager hook
    Signed-off-by: Damian Raczkowski <idontknow5325@gmail.com>

--- App.js
+++ App.js
@@ -9,13 +9,12 @@ import { QueryClient } from "@tanstack/react-query";
 import { PersistQueryClientProvider } from "@tanstack/react-query-persist-client";
 import { createAsyncStoragePersister } from "@tanstack/query-async-storage-persister";
 import AsyncStorage from "@react-native-async-storage/async-storage";
-
+import { useOnlineManager } from "./src/hooks/useOnlineManager";
 const queryClient = new QueryClient({
   defaultOptions: {
     queries: {
       cacheTime: 1000 * 60 * 60 * 24, // 24 hours,
       staleTime: 2000,
-      retry: 0,
     },
   },
 });
@@ -23,6 +22,8 @@ const asyncStoragePersister = createAsyncStoragePersister({
   storage: AsyncStorage,
 });
 export default function Main() {
+  useOnlineManager();
+
   return (
     <PersistQueryClientProvider
       client={queryClient}
