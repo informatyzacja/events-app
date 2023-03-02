1b737318488429e27595269ac89cc49aa8efcc8a
Michał Korczak <michal.k2001@gmail.com>
2023-03-02 01:01:44 +0100

    init turbo

---
+++ apps/web/src/pages/index.tsx
@@ -0,0 +1,18 @@
+import {
+  Container,
+  VStack,
+} from "@chakra-ui/react";
+import { type NextPage } from "next";
+
+const Home: NextPage = () => {
+  return (
+    <>
+      <Container maxW="container.xl">
+        <VStack w="100%" align="center">
+        </VStack>
+      </Container>
+    </>
+  );
+};
+
+export default Home;
