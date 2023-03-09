29bc2f4b2b972dce58735e5c3c83a4282c8485e0
Damian Raczkowski <idontknow5325@gmail.com>
2023-03-09 17:44:50 +0100

    Feature: Add entry logic for push notifications
    Signed-off-by: Damian Raczkowski <idontknow5325@gmail.com>

--- apps/expo/App.js
+++ apps/expo/App.js
@@ -1,15 +1,16 @@
-import { AppRegistry, SafeAreaView } from "react-native";
-import { Provider as PaperProvider } from "react-native-paper";
-import { NavigationContainer } from "@react-navigation/native";
-import { name as appName } from "./app.json";
-import theme from "./src/theme";
-import { StyleSheet } from "react-native";
-import Navbar from "./src/routes/Navbar";
-import { QueryClient } from "@tanstack/react-query";
-import { PersistQueryClientProvider } from "@tanstack/react-query-persist-client";
-import { createAsyncStoragePersister } from "@tanstack/query-async-storage-persister";
-import AsyncStorage from "@react-native-async-storage/async-storage";
-import { useOnlineManager } from "./src/hooks/useOnlineManager";
+import { AppRegistry, SafeAreaView } from 'react-native';
+import { Provider as PaperProvider } from 'react-native-paper';
+import { NavigationContainer } from '@react-navigation/native';
+import { name as appName } from './app.json';
+import theme from './src/theme';
+import { StyleSheet } from 'react-native';
+import Navbar from './src/routes/Navbar';
+import { QueryClient } from '@tanstack/react-query';
+import { PersistQueryClientProvider } from '@tanstack/react-query-persist-client';
+import { createAsyncStoragePersister } from '@tanstack/query-async-storage-persister';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import { useOnlineManager } from './src/hooks/useOnlineManager';
+
 const queryClient = new QueryClient({
   defaultOptions: {
     queries: {
--- apps/expo/src/hooks/useSubscribedEvents.js
+++ apps/expo/src/hooks/useSubscribedEvents.js
@@ -1,18 +1,47 @@
-import { subscribedEventsAtom } from "../atoms/subscribedEvents";
-import { useAtom } from "jotai";
+import { subscribedEventsAtom } from '../atoms/subscribedEvents';
+import { useAtom } from 'jotai';
+import * as Notifications from 'expo-notifications';
+// TODO: Add iteration on
+const scheduleNotification = async (event) =>
+  await Notifications.scheduleNotificationAsync({
+    content: {
+      title: event.name,
+      body: `Masz wydarzenie odbywające się w miejscu: ${event.place}`,
+      sound: false,
+      priority: Notifications.AndroidNotificationPriority.HIGH,
+      color: 'blue',
+    },
+    trigger: new Date(event.time), // TODO: change body of dummy_data to see if it works properly
+  });
+
+const removeNotification = async (event) => {
+  await Notifications.cancelScheduledNotificationAsync(event.notificationId);
+};
 
 export const useSubscribedEvents = () => {
   const [subscribedEvents, setSubscribedEvents] = useAtom(subscribedEventsAtom);
 
   const subscribe = (event) => {
+    const notificationId = scheduleNotification(event);
+    event.notificationId = notificationId;
+    event.subEvents.map((subEvent) => {
+      const subEventNotificationId = scheduleNotification(subEvent);
+      subEvent.notificationId = subEventNotificationId;
+    });
     const newSubscribedEvents = [...subscribedEvents, event];
     setSubscribedEvents(newSubscribedEvents);
   };
 
   const unSubscribe = (event) => {
-    const newSubscribedEvents = subscribedEvents.filter(
-      (item) => item.id !== event.id
-    );
+    const newSubscribedEvents = subscribedEvents.filter((item) => {
+      if (item.id === event.id) {
+        removeNotification(event);
+        event.subEvents.map((subEvent) => {
+          removeNotification(subEvent);
+        });
+      }
+      return item.id !== event.id;
+    });
     setSubscribedEvents(newSubscribedEvents);
   };
 
